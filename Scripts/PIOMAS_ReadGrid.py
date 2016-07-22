71d99e473f5ede9bf016decc615de99ae0b33987
John Fay <John.Fay@duke.edu>
2016-07-22 14:58:43 +1000

    Fixed dimension error and clips to mask

--- Scripts/PIOMAS_ReadGrid.py
+++ Scripts/PIOMAS_ReadGrid.py
@@ -3,7 +3,7 @@
 # Reads data in from two sets of files: grid.dat.pop and the monthly
 #  sea ice velocity vectors (u,v) for years 1979-2013, and converts
 #  these data into multipoint feature classes and then interpolated
-#  raster datasets in the EASE projection.
+#  (spline) raster datasets in the EASE projection.
 #
 # Data are from the Polar Science Center's PIOMAS project site 
 #  (http://psc.apl.uw.edu/research/projects/arctic-sea-ice-volume-anomaly/data/model_grid)
@@ -13,20 +13,20 @@
 #  ftp://pscftp.apl.washington.edu/zhang/PIOMAS/utilities/grid.dat.pop and
 #  ftp://pscftp.apl.washington.edu/zhang/PIOMAS/data/v2.1/other/
 #
-# Files are saved in the 
+# Output files are saved in the "Processed" folder
 #
 # July 2015
 # John.Fay@duke.edu
 
 debug = True
-createASCII = True
-writeRasters = False
+createASCII = False
+writeRasters = True
 
 import sys, os, gzip
 import numpy as np
 import arcpy
 
-#Folders
+#Get folder locations, relative to this script (which should be in scripts folder)
 scriptDir = os.path.dirname(sys.argv[0])
 rootDir = os.path.dirname(scriptDir)
 dataDir = os.path.join(rootDir,"Data","PolarScienceCenter","PIOMAS")
@@ -34,11 +34,17 @@ scratchDir = os.path.join(rootDir,"Scratch","Scratch.gdb")
 rawDir = os.path.join(dataDir,"RawData")
 outDir = os.path.join(dataDir,"Processed")
 
+#Get extent feature class and raster
+extentFC = os.path.join(rootDir,"Data","General","MaskPoly.shp")
+extentGrd = os.path.join(rootDir,"Data","General","StudyArea.img")
+maskRaster = os.path.join(rootDir,"Data","General","PIOMAS_Mask.img")
+arcpy.env.snapRaster = maskRaster
+
 #ArcPy setup
 arcpy.CheckOutExtension('spatial')
-arcpy.env.overwriteOutput = 1
+arcpy.env.overwriteOutput = True
 arcpy.env.scratchWorkspace = scratchDir
-srWGS84 = arcpy.SpatialReference(4326)
+srWGS84 = arcpy.SpatialReference(4326)#WGS 84
 srEASE = arcpy.SpatialReference(3408) #NSIDC_EASE_Grid_North
 
 #Setup for PIOMAS data
@@ -68,7 +74,7 @@ with open(os.path.join(rawDir,"grid.dat.pop"),'r') as grdFile:
 print "Slicing data into lat/long/angle arrays"
 latArr = flatArr[0,:,:]
 lngArr = flatArr[1,:,:]
-##HTNArr = flatArr[2,:,:] #Lenghts of sides of grid cell in KM (NESW)
+##HTNArr = flatArr[2,:,:] #Lengths of sides of grid cell in KM (NESW)- NOT USED
 ##HTEArr = flatArr[3,:,:]
 ##HUSArr = flatArr[4,:,:]
 ##HUWArr = flatArr[5,:,:]
@@ -90,8 +96,11 @@ for year in range(2013,2014):
         print "...processing month {}".format(strMonth)
         
         #Get the data for the current month
-        sliceU = flatArr[month,:,:]
-        sliceV = flatArr[month+120,:,:]
+        ##There are 20 records for each month; the 1st 10 are u values for the 10 depths
+        ##and the remaining 10 are v values for the 10 depths. We just one the 1st depth
+        ##so we want 0,20,40,...,220 for u values and 10,30,50,...230 for v values. 
+        sliceU = flatArr[month*20,:,:]
+        sliceV = flatArr[month*20 + 10,:,:]
         
         #Create the output file
         outFN = "Pts{}{}.shp".format(year,strMonth)
@@ -129,12 +138,18 @@ for year in range(2013,2014):
         #Interpolate U and V values to raster
         print "   ...Interpolating to raster"
         #Create u and v rasters
-        uBand = arcpy.sa.NaturalNeighbor(outFC2,"U",25000)
-        uBand.save()
-        vBand = arcpy.sa.NaturalNeighbor(outFC2,"V",25000)
+        uBand = outSpline = arcpy.sa.Spline(outFC2,"U",25000, "REGULARIZED", 0.1)
+        vBand = outSpline = arcpy.sa.Spline(outFC2,"V",25000, "REGULARIZED", 0.1)
+        ##uBand = arcpy.sa.NaturalNeighbor(outFC2,"U",25000)
+        ##vBand = arcpy.sa.NaturalNeighbor(outFC2,"V",25000)
         #Set zeros to NoData
-        uBand2 = arcpy.sa.SetNull(uBand,uBand,"Value = 0")
-        vBand2 = arcpy.sa.SetNull(vBand,vBand,"Value = 0")
+        ##arcpy.env.mask = extentGrd
+        ##uBand2 = arcpy.sa.SetNull(uBand,uBand,"Value = 0")
+        ##vBand2 = arcpy.sa.SetNull(vBand,vBand,"Value = 0")
+        #Remove data outside of mask
+        uBand2 = arcpy.sa.ExtractByMask(uBand,maskRaster)
+        vBand2 = arcpy.sa.ExtractByMask(vBand,maskRaster)
+             
         #Convert to ASCII files
         if createASCII:
             print "    ...Saving as ASCII files"
@@ -144,8 +159,7 @@ for year in range(2013,2014):
             vArr = arcpy.RasterToNumPyArray(vBand2,nodata_to_value=0)
             np.savetxt(outUFN,uArr,fmt='%8.6f')
             np.savetxt(outVFN,vArr,fmt='%8.6f')
-        #Create mask
-        mask = arcpy.sa.SetNull(vBand,1,"Value <> 0")
+                       
         #Composite to output raster and set projection to EASE
         if writeRasters:
             outRaster = os.path.join(outDir,"uv{}{}.img".format(year,strMonth))
