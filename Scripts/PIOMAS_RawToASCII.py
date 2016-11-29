fe17afb1078d0484becede5db0c1929f8e213ea6
John Fay <john.fay@duke.edu>
2016-11-29 13:26:39 -0500

    Updates RawToASCII.py to use Jesse's corrections

--- Scripts/PIOMAS_RawToASCII.py
+++ Scripts/PIOMAS_RawToASCII.py
@@ -1,9 +1,8 @@
-#PIOMAS_RawToASCII.py
+#PIOMAS_RawToPointFeatures.py
 #
 # Reads data in from two sets of files: grid.dat.pop and the monthly
 #  sea ice velocity vectors (u,v) for years 1979-2013, and converts
-#  these data into multipoint feature classes and then interpolated
-#  (spline) raster datasets in the EASE projection.
+#  these data into multipoint feature classes in the EASE projection.
 #
 # Data are from the Polar Science Center's PIOMAS project site 
 #  (http://psc.apl.uw.edu/research/projects/arctic-sea-ice-volume-anomaly/data/model_grid)
@@ -13,20 +12,19 @@
 #  ftp://pscftp.apl.washington.edu/zhang/PIOMAS/utilities/grid.dat.pop and
 #  ftp://pscftp.apl.washington.edu/zhang/PIOMAS/data/v2.1/other/
 #
-# Output files are saved in the "Processed" folder and are constrained to the extent
-#  of the PIOMAS_Mask.img raster dataset, which was created by creating a 1-NoData mask
-#  from values in a fully interpolated output clipped to the extent of the MaskPoly.shp
-#  feature class.
+# Output files are saved in the "Processed" folder 
 #
-# July 2015
+# November 2015
 # John.Fay@duke.edu
 
 import sys, os, gzip, arcpy
 import numpy as np
 
 #Set output options
+startYear = 2013
+endYear = 2014
 createASCII = True
-writeRasters = False
+writeRasters = True
 
 #Get folder locations, relative to this script (which should be in scripts folder)
 ##Input folders
@@ -36,20 +34,20 @@ dataDir = os.path.join(rootDir,"Data","PolarScienceCenter","PIOMAS")    #Root fo
 rawDir = os.path.join(dataDir,"RawData")                                #contains raw PIOMAS files
 
 ##Output folders
-scratchDir = os.path.join(rootDir,"Scratch","Scratch.gdb")              #To hold scratch/temporary files
-rawDir = os.path.join(dataDir,"RawData")                                #Folder containing raw PIOMAS files
+scratchDir = os.path.join(rootDir,"Scratch","Scratch.gdb")  #To hold scratch/temporary files
+rawDir = os.path.join(dataDir,"RawData")                    #Folder containing raw PIOMAS files
 outDir = os.path.join(dataDir,"Processed")
-pointFCDir = os.path.join(dataDir,"Processed","PointFeatures")          #To hold output point files
-rasterDir = os.path.join(dataDir,"Processed","Raster")                  #To hold output raster files
-ASCIIDir = os.path.join(dataDir,"Processed","ASCII")                    #To hold output ASCII u and v files (monthly)
+pointFCDir = os.path.join(outDir,"PointFeatures")           #To hold output point filesrasterDir = os.path.join(dataDir,"Processed","Raster")                  #To hold output raster files
+rasterDir = os.path.join(outDir,"Raster")                   #To hold output raster files
+ASCIIDir = os.path.join(outDir,"ASCII")                     #To hold output ASCII u and v files (monthly)
 
-##Create output folders, if not present
+#Create output folders, if not present
 for theDir in (outDir,pointFCDir,rasterDir,ASCIIDir):
     if not os.path.exists(theDir):
         print "Creating {}".format(theDir)
         os.mkdir(theDir)
 
-##Create scratch file geodatabase, if not present
+#Create scratch file geodatabase, if not present
 if not os.path.exists(scratchDir):
     arcpy.CreateFileGDB_management(os.path.dirname(scratchDir),"Scratch.gdb")
 
@@ -69,6 +67,7 @@ xDim = 120
 yDim = 360
 varType = np.float32
 
+##-PROCESSING--------------------------------------------------------------------------------------
 #Read the grid.dat.pro data containing lat,long, and angle data
 with open(os.path.join(rawDir,"grid.dat.pop"),'r') as grdFile:
     print "Reading in grid.data.pop data"
@@ -84,8 +83,12 @@ lngArr = flatArr[1,:,:]
 ##HUWArr = flatArr[5,:,:]
 anglArr = flatArr[6,:,:] #Angle
 
+#Read in EASE correction values
+easeCSV = os.path.join(rawDir,'EASEcorrection.csv')
+easeArr = np.loadtxt(easeCSV).reshape(yDim,xDim)
+
 #Loop through years
-for year in range(1978,2014):
+for year in range(startYear,endYear):
     print "Processing data for {}".format(year)
     yearFN = os.path.join(rawDir,"uo1_10.H{}.gz".format(year))
     #Read the entire annual data (all levels) into array
@@ -96,7 +99,7 @@ for year in range(1978,2014):
         ## the 1st dimension is further subset into: Month/Depth Level/U-V slices
 
     #Loop through the 12 months of data in the yearly data file
-    for month in range(12):
+    for month in range(1):
         strMonth = str(month+1).zfill(2)
         print "...processing month {}".format(strMonth)
         
@@ -112,33 +115,61 @@ for year in range(1978,2014):
         outFC2 = os.path.join(outDir,"PointFeatures",outFN)
         if arcpy.Exists(os.path.join(outDir,outFC2)):
             print "Already created, skipping."
-            continue
+            #continue
         print "   ...Creating point file for month: {}".format(strMonth)
         outFC = arcpy.CreateFeatureclass_management("in_memory","tmp","POINT",spatial_reference=srWGS84)
         
         #Add fields
         print "   ...Adding fields"
+        arcpy.AddField_management(outFC,"Lat","FLOAT",10,8)
+        arcpy.AddField_management(outFC,"Lng","FLOAT",10,8)
         arcpy.AddField_management(outFC,"Angle","FLOAT",8,2)
-        arcpy.AddField_management(outFC,"U","FLOAT",8,2)
-        arcpy.AddField_management(outFC,"V","FLOAT",8,2)
+        arcpy.AddField_management(outFC,"U","FLOAT",8,2)            #Native U value (GOCC)
+        arcpy.AddField_management(outFC,"V","FLOAT",8,2)            #Native V value (GOCC)
+        arcpy.AddField_management(outFC,"NearAngle","FLOAT",8,2)    #Near angle between PIOMAS point and GOCC center
+        arcpy.AddField_management(outFC,"NAT_Dir","FLOAT",8,2)      #Bearing based on Zhangs U/V
+        arcpy.AddField_management(outFC,"NAT_Mag","FLOAT",8,2)      #Magnitude based on Zhangs U/V
+        arcpy.AddField_management(outFC,"EASE_Dir","FLOAT",8,2)     #Adjusted bearing (EASE)
+        arcpy.AddField_management(outFC,"EASE_U","FLOAT",8,2)           #Adjusted U (EASE)
+        arcpy.AddField_management(outFC,"EASE_V","FLOAT",8,2)           #Adjusted V (EASE)
 
         #Loop through each data point and add ad features to the output feature class
-        cursor = arcpy.da.InsertCursor(outFC,['SHAPE@XY','Angle','U','V'])
+        cursor = arcpy.da.InsertCursor(outFC,['SHAPE@XY','Lat','Lng','Angle','NearAngle','U','V',
+                                              "NAT_Dir","NAT_Mag","EASE_Dir","EASE_U","EASE_V"])
         for x in range(yDim):
             for y in range(xDim):
                 theLat = latArr[x,y]
                 theLng = lngArr[x,y]
                 theAngle = anglArr[x,y]
-                theU = sliceU[x,y]
-                theV = sliceV[x,y]
-                theRec = ((theLng,theLat),theAngle,theU,theV)
+                U = sliceU[x,y] 
+                V = sliceV[x,y]
+                nearAngle = easeArr[x,y]    #Angle from EASRCorrection.csv
+                
+                #If the U and V are both zero, do not add the point
+                if U == 0 and V == 0: continue
+                
+                #Compute the bearings (in degrees) in GOCC from U and V 
+                NAT_DIR = math.degrees(math.atan2(V,U)) 
+                EASE_DIR = NAT_DIR - 90 + nearAngle
+                
+                #Compute the magnitude (Pythagorean theorem)
+                magnitude = math.sqrt(U**2 + V**2)
+                
+                #Decompose bearing 2 back into U and V
+                EASE_U = math.cos(math.radians(EASE_DIR))*magnitude
+                EASE_V = math.sin(math.radians(EASE_DIR))*magnitude
+                
+                #Write values to the table and insert the row
+                theRec = ((theLng,theLat),theLat,theLng,theAngle,nearAngle,U,V,
+                          NAT_DIR,magnitude,EASE_DIR,EASE_U,EASE_V)
                 cursor.insertRow(theRec)
+
         del cursor
         
         #Reproject to EASE grid
-        print "   ...reprojecting to EASE projection"
+        print "   ...Reprojecting to EASE projection"
         outFC2 = arcpy.Project_management(outFC,outFC2,srEASE)
-    
+
         #Interpolate U and V values to raster
         print "   ...Interpolating to raster"
         #Create u and v rasters via Spline method
@@ -161,7 +192,7 @@ for year in range(1978,2014):
                        
         #Composite to output raster and set projection to EASE
         if writeRasters:
-            outRaster = os.path.join(outDir,"uv_{}_{}.img".format(year,strMonth))
+            outRaster = os.path.join(rasterDir,"uv_{}_{}.img".format(year,strMonth))
             arcpy.CompositeBands_management((uBand2,vBand2),outRaster)
             arcpy.DefineProjection_management(outRaster,srEASE)
         
@@ -170,3 +201,6 @@ for year in range(1978,2014):
         arcpy.Delete_management(vBand)
         arcpy.Delete_management(uBand2)
         arcpy.Delete_management(vBand2)
+
+        
+        
